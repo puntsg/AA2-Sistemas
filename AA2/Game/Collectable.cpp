f4b210e028fa4a9d13788cab0ef9e5eae409ffdb
DESKTOP-10JPS7V\sergi <sergigonzalezberlanga@gmail.com>
2024-11-26 17:32:41 +0100

    Collectables

---
+++ AA2/Game/Collectable.cpp
@@ -0,0 +1,16 @@
+#include "Collectable.h"
+#include <iostream>
+
+void Collectable::Draw(Vector2 offset)
+{
+	std::cout << "O";
+}
+
+void Collectable::Collect(Player* player)
+{
+	int random = rand() % 100;
+	if (random % 2)
+		player->potions++;
+	else
+		player->coins++;
+}
---
+++ AA2/Game/Collectable.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "../3Nodes/Vector2.h"
+#include "../3Nodes/INodeContent.h"
+#include "Player.h"
+class Collectable : public INodeContent {
+public:
+	void Draw(Vector2 offset) override;
+	void Collect(Player* player);
+};
